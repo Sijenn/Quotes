0c33be26b9a1b9e147af2322d1ec7fb7530eb3bc
jenny <jenniffer>
2019-07-08 13:40:14 +0300

    generate component quote and date-count pipe

---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,42 @@
+
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+  quotes: Quote[] = 
+  [
+    
+  ];
+  addNewQuote(quote){
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength+1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
+  }
+  
+  toggleDetails(index){
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
+  deleteQuote(isComplete, index){
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ?`)
+
+      if (toDelete){
+        this.quotes.splice(index,1)
+      }
+    }
+  }
+  
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+}
